4c92711f7c03717f817c3fd47ba1e6f0bf7a93c5
yfzhang <nankaihunter@gmail.com>
2012-02-14 23:59:54 +0800

    implement basic structure, including Database design, puzzle list show & puzzle view

---
+++ src/zyf/game/wordpuzzle/SettingActivity.java
@@ -0,0 +1,12 @@
+package zyf.game.wordpuzzle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.setting);
+	}
+}
